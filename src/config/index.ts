5df2f2290c97049cbcfe0a1d2268158e41029700
Andrew Shearer <andrewshearerdev@gmail.com>
2025-07-09 15:02:39 -0700

    installed dotenv package
    - updated config/index.ts to load environment variables
    - added Type interface for config as well

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,11 @@
-export const config = {
-  port: process.env.PORT || 4000,
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface Config {
+  port: number;
+}
+
+export const config: Config = {
+  port: Number(process.env.PORT) || 4000,
 };
