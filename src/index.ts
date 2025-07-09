52784e052a41bc9c74369bc43e1d21ebef486f8b
Andrew Shearer <andrewshearerdev@gmail.com>
2025-07-09 13:14:19 -0700

    added some basic app.get calls for /api/dogs

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,27 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api", (req: Request, res: Response) => {
   res.send("Hello, world!");
+});
+
+const dogsData = [
+  { id: 1, name: "Buddy", breed: "Golden Retriever" },
+  { id: 2, name: "Max", breed: "Beagle" },
+  { id: 3, name: "Bella", breed: "Bulldog" },
+  { id: 4, name: "Lucy", breed: "Poodle" },
+  { id: 5, name: "Charlie", breed: "Labrador" },
+];
+
+app.get("/api/dogs", (req: Request, res: Response) => {
+  res.json(dogsData);
+});
+
+app.get("/api/dogs/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const matchingDog = dogsData.find((dog) => dog.id === Number(id));
+
+  res.json(matchingDog || { error: "Dog not found" });
 });
 
 app.listen(port, () => {
